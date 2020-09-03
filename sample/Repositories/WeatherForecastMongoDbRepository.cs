5bd77df1de9b209e2c2ae73a97ce76107a7befe8
Timm <timm@bremekamp.com>
2020-09-03 12:20:02 +0200

    Renamed WeatherForecastRepository due to naming conventions

---
+++ sample/Repositories/WeatherForecastMongoDbRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Options;
+using MongoRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.Repositories
+{
+    public class WeatherForecastMongoDbRepository : ReadWriteRepository<WeatherForecast, string>, IWeatherForecastRepository
+    {
+        public WeatherForecastMongoDbRepository(IOptions<MongoDbOptions> mongoOptions) : base(mongoOptions)
+        {
+
+        }
+    }
+}
